7b7c48eba3dd237895e7cd0c5495f359130e79e7
João Vitor <jvde.olivera@gmail.com>
2025-04-14 23:56:29 -0300

    adicionando tela: como gostaria de transferir

---
+++ app/src/main/java/com/example/provaigor/como_gostaria_de_transferir.kt
@@ -0,0 +1,16 @@
+package com.example.provaigor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class como_gostaria_de_transferir : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_como_gostaria_de_transferir)
+
+    }
+}
