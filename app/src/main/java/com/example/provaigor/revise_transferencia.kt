610d8d72b5a11b3497a2fa6f000c16dd2c0e62f6
João Vitor <jvde.olivera@gmail.com>
2025-04-15 00:33:05 -0300

    adicionando tela: revise sua transferencia

---
+++ app/src/main/java/com/example/provaigor/revise_transferencia.kt
@@ -0,0 +1,16 @@
+package com.example.provaigor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class revise_transferencia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_revise_transferencia)
+
+    }
+}
