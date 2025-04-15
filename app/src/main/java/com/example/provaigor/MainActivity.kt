9389553399447212742f232bddc5cf611490016f
João Vitor <jvde.olivera@gmail.com>
2025-04-14 22:19:01 -0300

    Manda pro git pfv

---
+++ app/src/main/java/com/example/provaigor/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.provaigor
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_principal, menu)
+        return true
+    }
+}
---
+++ app/src/main/java/com/example/provaigor/contatos_transferir.kt
@@ -0,0 +1,16 @@
+package com.example.provaigor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class contatos_transferir : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_contatos_transferir)
+
+    }
+}
